5dfa3dc4e5887d44238f4ea9548152d4bdd425b0
AJ <amanda.elisabet.jordan@gmail.com>
2019-01-20 23:34:19 -0500

    almost there!

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1 />
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Route path='/' exact component={Landing} />
+          <Route path='/register/' component={Register} />
+          <Route path='/login/' component={Login} />
+        </div>
+      </Router>
     );
   }
 }
