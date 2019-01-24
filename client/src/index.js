54f0dbbe31968e06102e0d51c1325d6de8e1f33d
AJ <amanda.elisabet.jordan@gmail.com>
2019-01-24 14:03:04 -0500

    it works!

--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
 import App from './App';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authActions';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// check for token in local storage to keep user logged in
-if (localStorage.jwtToken) {
-  // set axios auth token header
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // decode token and get user info and exp
-  const decoded = jwt_decode(token);
-
-  // store user info and auth status in Redux store
-  store.dispatch(setCurrentUser(decoded));
-
-  //check if auth token is expired; if so, redirect to login
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-
-    window.location.href = './login';
-  }
-}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
